3b2be041353cdbe9744d6041bfdca394ae6bf523
jowjow22 <jonathan224santos@gmail.com>
2023-09-17 18:05:38 -0300

    feat: initiate A_star algorithm

--- Assets/Scripts/GhostChase.cs
+++ Assets/Scripts/GhostChase.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
 using Mono.Cecil;
 using UnityEngine;
 
 public class GhostChase : GhostBehavior
 {
-    public TypeChase specificChase { get; private set; }
+
 
     private void OnDisable()
     {
@@ -11,11 +12,103 @@ public class GhostChase : GhostBehavior
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        specificChase.Chase(other, ghost);
+        this.Chase(other, ghost);
     }
 
-    private void OnEnable()
+
+
+    public void Chase(Collider2D other, Ghost ghost)
     {
-        specificChase = GetComponent<TypeChase>();
+        switch (ghost.name)
+        {
+            case "Ghost_Blinky":
+                this.ChaseAStar(other, ghost);
+                break;
+            case "Ghost_Pinky":
+                this.ChaseFront(other, ghost);
+                break;
+            case "Ghost_Inky":
+                this.GhostChaseDefault(other, ghost);
+                break;
+            case "Ghost_Clyde":
+                this.ChaseFront(other, ghost);
+                break;
+        }
+    }
+    private void ChaseFront(Collider2D other, Ghost ghost)
+    {
+        Node node = other.GetComponent<Node>();
+
+        if (node != null && enabled && !ghost.frightened.enabled)
+        {
+            Vector2 direction = Vector2.zero;
+            float minDistance = float.MaxValue;
+
+            foreach (Vector2 avaliableDirection in node.availableDirections)
+            {
+                Vector3 newPosition = transform.position + new Vector3(avaliableDirection.x, avaliableDirection.y, 0f);
+
+
+
+                Vector3 frontOfTarget = ghost.target.position + new Vector3(ghost.target.GetComponent<Movement>().direction.x * 6f, ghost.target.GetComponent<Movement>().direction.y * 6f, 0f);
+
+                float distance = (frontOfTarget - newPosition).sqrMagnitude;
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    direction = avaliableDirection;
+                }
+            }
+
+            ghost.movement.SetDirection(direction);
+        }
+    }
+
+    private void GhostChaseDefault(Collider2D other, Ghost ghost)
+    {
+        Node node = other.GetComponent<Node>();
+
+        if (node != null && enabled && !ghost.frightened.enabled)
+        {
+            Vector2 direction = Vector2.zero;
+            float minDistance = float.MaxValue;
+
+            foreach (Vector2 avaliableDirection in node.availableDirections)
+            {
+                Vector3 newPosition = transform.position + new Vector3(avaliableDirection.x, avaliableDirection.y, 0f);
+
+                float distance = (ghost.target.position - newPosition).sqrMagnitude;
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    direction = avaliableDirection;
+                }
+            }
+
+            ghost.movement.SetDirection(direction);
+        }
+    }
+
+    private void ChaseAStar(Collider2D other, Ghost ghost)
+    {
+        Node node = other.GetComponent<Node>();
+
+        List<Node> openList = new List<Node>();
+        List<Node> closedList = new List<Node>();
+
+        if (node != null && enabled && !ghost.frightened.enabled)
+        {
+            foreach (Vector2 avaliableDirection in node.availableDirections)
+            {
+                Debug.Log("Avaliable Direction: " + avaliableDirection);
+                Node nextNode = node.GetNextNode(avaliableDirection);
+                if (nextNode != null)
+                {
+                    openList.Add(nextNode);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,14 +1,17 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Node : MonoBehaviour
 {
     public LayerMask obstacleLayer;
-    public List<Vector2> availableDirections { get; private set;}
+    public LayerMask nodeLayer;
+    public List<Vector2> availableDirections { get; private set; }
 
-    private void Start(){
+    private void Start()
+    {
         this.availableDirections = new List<Vector2>();
-        
+
         CheckAvaliableDirection(Vector2.up);
         CheckAvaliableDirection(Vector2.down);
         CheckAvaliableDirection(Vector2.left);
@@ -16,10 +19,29 @@ public class Node : MonoBehaviour
 
     }
 
-    private void CheckAvaliableDirection(Vector2 direction){
+    private void CheckAvaliableDirection(Vector2 direction)
+    {
         RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.5f, 0.0f, direction, 1.0f, this.obstacleLayer);
-        if(hit.collider == null){
+        if (hit.collider == null)
+        {
             this.availableDirections.Add(direction);
         }
+    }
+    public Node GetNextNode(Vector2 direction)
+    {
+        RaycastHit2D[] hits;
+        hits = Physics2D.RaycastAll(transform.position, direction, float.MaxValue, nodeLayer);
+
+        Boolean hasFoundOne = false;
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hasFoundOne)
+            {
+                return hit.collider.gameObject.GetComponent<Node>();
+            }
+            hasFoundOne = true;
+        }
+
+        return null;
     }
 }
